ef605474db381650f1c91908fa6a68e65ce00026
Jason Leach <jlspursfan@gmail.com>
2019-05-23 01:28:18 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    map { |k, v| arguments.include?(v) ? k : nil }.compact
+    #binding.pry
+    #arr = []
+    #if
+  #    arr << key
+  #  end
+  #  return arr
+
+
   end
 end
